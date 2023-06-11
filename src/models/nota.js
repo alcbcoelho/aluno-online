8fcb94bbe4aaab50cf7ca98e6d9618bb737df503
André <alcbcoelho@gmail.com>
2023-06-11 20:54:20 -0300

    Implementado teste unitário para função mediaCA (src/models/tests/mediaCA.test.js), da classe Nota (src/models/nota.js)

--- src/models/nota.js
+++ src/models/nota.js
@@ -15,7 +15,12 @@ class Nota {
     }
 
     mediaCA() {
-        return "SS";
+        if (this.mediaFinal() >= 9 && this.mediaFinal() <= 10) return "SS";
+        if (this.mediaFinal() >= 7 && this.mediaFinal() <= 8.9) return "MS";
+        if (this.mediaFinal() >= 5 && this.mediaFinal() <= 6.9) return "MM";
+        if (this.mediaFinal() >= 3 && this.mediaFinal() <= 4.9) return "MI";
+        if (this.mediaFinal() >= 0.1 && this.mediaFinal() <= 2.9) return "II";
+        if (this.mediaFinal() <= 0.0) return "SR";
     }
 }
 
